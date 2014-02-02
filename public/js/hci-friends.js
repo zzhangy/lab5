d87044d8c27b4bf87d3a588e5f63f4b8f243f3b4
Michael Bernstein <msb@cs.stanford.edu>
2014-02-01 20:50:07 -0800

    anagramming framework

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -11,3 +11,30 @@ $(document).ready(function() {
 function initializePage() {
 	console.log("Javascript connected!");
 }
+
+function anagrammedName(name) {
+	// Thanks, Internet Anagram Server!
+	
+	if (name == "Doug Engelbart") {
+		return "Notable Grudge";
+	} 
+	else if (name == "Ivan Sutherland") {
+		return "Vandal Heist Run";
+	}
+	else if (name == "JCR Licklider") {
+		return "Crick Rid Jell";
+	}
+	else if (name == "Vannevar Bush") {
+		return "Ravens Van Hub";
+	}
+	else if (name == "Alan C. Kay") {
+		return "Canal Yak";
+	}
+	else if (name == "Allen Newell") {
+		return "Ellen All New";
+	}		
+	else {
+		console.log(name + " not known for anagramming.");
+		return name;
+	}
+}
