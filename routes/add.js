67ca143e6e6458f6fe3e2195175a428dc59287eb
Yalin Zhang <theyalinzhang@gmail.com>
2014-02-06 11:51:39 -0800

    update for end of lab

--- routes/add.js
+++ routes/add.js
@@ -1,7 +1,15 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) { 
-	// Your code goes here
-	console.log(data);
-	//res.render('index', data);
+	// create friend
+	var name = req.query.name;
+	var des = req.query.description;
+	var img = 'http://lorempixel.com/400/400/people';
+	var newFriend = {'name' : name,
+				  'description' : des,
+				  'imageURL' : img};
+	console.log(newFriend);
+
+	data['friends'].push(newFriend);
+	res.render('add');
 }
